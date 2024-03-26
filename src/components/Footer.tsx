c8d381e7255cd1c6e5fbe7672eee94f65bc989c6
Daniel Rodriguez <danydeitu18@gmail.com>
2024-03-26 15:40:39 -0300

    add social net

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,32 +3,34 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer id="contacto" className="bg-gray-800 text-white py-8">
-        <div className="container items-center pb-2 pt-8 mb-6">
-       Podes contactarme por
-        </div>
-        <div className="container mx-auto flex justify-evenly "> 
-<div>
-        <a href="https://wa.me/+5401140941119" target="_blank" rel="noreferrer">
-            <i className="fab fa-whatsapp whatsapp"></i></a>
-        </div>
-        <div>
-        <a href="mailto:disenosdanyweb@gmail.com"
-            target="_blank"
-            rel="noreferrer">
-            <i className="fa fa-envelope"aria-hidden="true"></i>
-          </a>
-          </div>
-        <div>
-          <a href="https://www.linkedin.com/in/daniel-alberto-rodriguez/" className="mr-4"><i className="fab fa-linkedin linkedin"></i></a></div>
-          <div>
-          <a href="https://github.com/danydeitu/"><i className="fab fa-github github" ></i></a>
-        </div>
-  
+         <div className="container mx-auto flex items-center justify-center mt-4 mb-5">
+        <p>Mis Redes sociales</p>
       </div>
-      <div className="container items-center pb-2 pt-8">
-      
-      &copy; {new Date().getFullYear()} Daniel Rodriguez
-    </div>
+      <div className="container mx-auto flex items-center justify-center">
+        <div className="mr-6">
+          <a href="https://wa.me/+5401140941119" target="_blank" rel="noreferrer">
+            <i className="fab fa-whatsapp whatsapp"></i>
+          </a>
+        </div>
+        <div className="mr-6">
+          <a href="mailto:disenosdanyweb@gmail.com" target="_blank" rel="noreferrer">
+            <i className="fa fa-envelope" aria-hidden="true"></i>
+          </a>
+        </div>
+        <div className="mr-6">
+          <a href="https://www.linkedin.com/in/daniel-alberto-rodriguez/" target="_blank" rel="noreferrer">
+            <i className="fab fa-linkedin linkedin"></i>
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/danydeitu/" target="_blank" rel="noreferrer">
+            <i className="fab fa-github github"></i>
+          </a>
+        </div>
+      </div>
+      <div className="container mx-auto flex items-center justify-center mt-4">
+        <p>&copy; {new Date().getFullYear()} Daniel Rodriguez</p>
+      </div>
     </footer>
   );
 };
