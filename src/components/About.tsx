b805e2c4a8357d1dc8e74dc7792a05160a0fc883
Daniel Rodriguez <danydeitu18@gmail.com>
2024-04-17 21:03:46 -0300

    add dar

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Acerca: React.FC = () => {
   return (
-    <section id="acerca" className="py-1 bg-gray-50 dark:bg-black">
+    <section id="acerca" className="py-1 bg-white dark:bg-gray-900">
       <div className="container mx-auto flex flex-col md:flex-row items-center bg-white dark:bg-gray-900">
         <div className="md:w-1/2 md:order-2 md:text-right md:pr-8">
           <img src="./img/mifoto.jpg" alt="Descripción de la imagen" className="w-full md:w-auto" />
         </div>
-        <div className="md:w-1/2 md:order-1">
+        <div className="md:w-1/2 md:order-1dark:bg-gray-100 text-black dark:bg-gray-900 dark:text-white">
           <h2 className="text-3xl font-bold mb-4 text-center">Acerca de mí</h2>
           <p className="text-lg">
           Soy Desarrollador web Full Stack y Estudiante de Telecomunicaciones ,
